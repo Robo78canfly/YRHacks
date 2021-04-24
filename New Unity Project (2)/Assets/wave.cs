d66964defe21e8b9a883f4f0112fde70817d52c3
chinxed <76501452+chinxed@users.noreply.github.com>
2021-04-23 23:09:33 -0400

    enemy hp
    pog

--- New Unity Project (2)/Assets/wave.cs
+++ New Unity Project (2)/Assets/wave.cs
@@ -6,9 +6,13 @@ public class wave : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject Enemy;
+
+    // Number of regular red enemies to spawn
+    int redSpawn = 10;
+    int orangeSpawn = 5;
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < redSpawn; i++)
         {
             Instantiate(Enemy, new Vector3(-8.5f + (i * -2.0f), 0.75f, 0), Quaternion.identity);
         }
